e6be00478165e63c8baead3a68f08277db37ff29
Nickpodski <Nickpodski@gmail.com>
2021-04-17 04:08:50 -0400

    gitignore initial and server.js update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const mongoose = require("mongoose");
+const path = require('path');
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require('./routes');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000
 
@@ -10,19 +13,13 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Projects", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+mongoose.connect(process.env.MONGOD_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
-require("./routes/apiRoutes.js")(app)
-require("./routes/htmlRoutes.js")(app)
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
